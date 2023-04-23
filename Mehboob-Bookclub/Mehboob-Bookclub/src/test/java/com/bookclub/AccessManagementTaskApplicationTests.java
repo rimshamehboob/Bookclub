3dbdf100840760887d174e4b6d6212328888c789
rimshamehboob <114309006+rimshamehboob@users.noreply.github.com>
2023-04-24 02:33:56 +0300

    Add files via upload

---
+++ Mehboob-Bookclub/Mehboob-Bookclub/src/test/java/com/bookclub/AccessManagementTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessManagementTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Mehboob-Bookclub/src/main/java/com/bookclub/AccessManagementTaskApplication.java
@@ -0,0 +1,13 @@
+package com.bookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplicationcl
+public class AccessManagementTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessManagementTaskApplication.class, args);
+    }
+
+}
